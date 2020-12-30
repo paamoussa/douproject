b72514c26b77ca6cbbefe4309f3acfd551c90285
zertox-tech <pamodiallo@gmail.com>
2020-12-30 21:11:44 +0000

    Aime moi Doudou

---
+++ projetdoudou/src/main/java/com/tdsi/projetdoudou/ProjetdoudouApplication.java
@@ -0,0 +1,13 @@
+package com.tdsi.projetdoudou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetdoudouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetdoudouApplication.class, args);
+	}
+
+}
---
+++ projetdoudou/src/test/java/com/tdsi/projetdoudou/ProjetdoudouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdsi.projetdoudou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetdoudouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
